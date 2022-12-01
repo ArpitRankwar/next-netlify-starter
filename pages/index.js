3ab00c91885b7f7ebd3a9ee170726ca9006d295f
ArpitRankwar <arpitchaudhary.2001@gmail.com>
2022-12-01 14:05:31 +0530

    ch

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,24 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
 
 export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	return (
+		<div className="container">
+			<Head>
+				<title>Wiingy</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+			<main>
+				<Header title="Welcome to my app!" />
+				<p className="description">
+					Want to Learn Python{" "}
+					<a href="https://wiingy.com/python/">Click Here </a>
+				</p>
+			</main>
 
-      <Footer />
-    </div>
-  )
+			<Footer />
+		</div>
+	);
 }
